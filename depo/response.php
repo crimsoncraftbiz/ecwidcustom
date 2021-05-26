daaf314603746a896101cad9e84758bcac964a16
crimsoncraftbiz <crimsoncraft.supp@gmail.com>
2021-05-26 10:21:40 +0800

    php response

---
+++ depo/response.php
@@ -0,0 +1,22 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+$req = json_decode(file_get_contents("php://input"),true);
+​
+// Here you can check/process the order data to calculate surcharge
+​
+$surcharge = array(
+    "surcharges" => array(
+        array(
+            "id" => "my_custom_surcharge",
+            "value" => 5,
+            "type" => "PERCENT",
+            "description" => "5% for PayPal checkout"
+        )
+    )
+);
+​
+$output = json_encode($surcharge);
+echo $output;
+​
+?>
