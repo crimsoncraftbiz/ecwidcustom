73fa6716e71a49985e63cc5edd161edfca13767c
crimsoncraftbiz <crimsoncraft.supp@gmail.com>
2021-05-26 10:40:02 +0800

    Update response.php

--- response.php
+++ response.php
@@ -1,28 +1,5 @@
-<?php 
-header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json; charset=utf-8');
-$req = json_decode(file_get_contents("php://input"),true);
-​
-// Here you can check/process the order data to calculate surcharge
-​
-echo "My first PHP script!";
+<?php
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
 
-$surcharge = array(
-    "surcharges" => array(
-        array(
-            "id" => "my_custom_surcharge",
-            "value" => 5,
-            "type" => "PERCENT",
-            "description" => "5% for PayPal checkout"
-        )
-    )
-);
-​
-$output = json_encode($surcharge);
-echo $output;
-​
-$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
-
-echo json_encode($age);
-
+var_dump(json_decode($jsonobj, true));
 ?>
