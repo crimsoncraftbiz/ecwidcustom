3cb694b88d9a6a7d3985fbacf18ec53ce6888ab4
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-01 20:28:08 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -1,9 +1,9 @@
 Ecwid.OnPageLoaded.add(function(page) {
-	console.log('The custom app works!');
+	console.log('The custom app loaded');
 	
 	if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
 		//alert this app loaded
-		console.log('The custom app works second time!!');
+		console.log('The custom app works in payment page!!');
 		
 		// check current payment method
 		document.getElementsByClassName("ec-radiogroup__item--checked").onclick = function(){
