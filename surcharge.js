e7b6ad909acd9c857c071f07adab6e0a8c9a01de
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-02 23:27:17 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -9,5 +9,26 @@ Ecwid.OnPageLoaded.add(function(page) {
 		element.onclick = function(event) {
   			console.log(event);
 		};
+		
+		// Initialize extra fields
+		ec = ec || {};
+		ec.order = ec.order || {};
+		ec.order.extraFields = ec.order.extraFields || {};
+		// Set order surcharge
+		ec.order.extraFields.surcharge = {
+		    'value': 'Custom charge',
+		    "options": [
+		    { 
+			"title": "Custom charge",
+			"surcharge": 5
+		    },
+		    ],
+		    "surchargeShortName": {
+			"name": "Surcharge",
+			 "showSurchargePercentValue": false
+		    },
+		    'surchargeType': 'PERCENT'
+		}
+		Ecwid.refreshConfig();
 	}
 });
