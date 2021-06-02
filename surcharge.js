396f12d8275e9ad149f5caec947f8b8258e8cc3d
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-02 14:32:40 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -8,6 +8,6 @@ Ecwid.OnPageLoaded.add(function(page) {
 		// check current payment method
 		document.getElementsByclassName("ec-radiogroup__item ec-radiogroup__item--2889-1620485335547 ec-radiogroup__item--PayPal ec-radiogroup__item--module-PayPalStandard  ec-radiogroup__item--multiline").onclick = function () {
 			console.log('clicked');
-		});
+		};
 	}
 });
