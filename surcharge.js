f532635c5408b6b8bfcec6a93064134782838272
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-03 12:56:22 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -4,28 +4,39 @@ Ecwid.OnPageLoaded.add(function(page) {
 	if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
 		//alert this app loaded
 		console.log("Current page is of type: " + page.type);
-	
-	
-	
-		// Initialize extra fields
-		ec = ec || {};
-		ec.order = ec.order || {};
-		ec.order.extraFields = ec.order.extraFields || {};
+		
+		var x = 0;
+		
+		//Select payment define surcharge percent
+		document.getElementByClassName("form-control__radio").addEventListener("click", paypalextra);
 
-		ec.order.extraFields.surcharge = {
-			'value': 'Custom charge',
-			'options': [{ 
-				"title": "Credit Card Processing Fee",
-				"surcharge": 5
-			},
-			],
-			'surchargeShortName': {
-			    'name': 'ProcessingFee',
-			    'showSurchargePercentValue': false
-			},
-			'surchargeType': 'PERCENT',
-			'showZeroSurchargeInTotal': false
-		};
-		Ecwid.refreshConfig();	
+		function paypalextra() {
+			x = 5;
+			console.log("surcharge : " + x);
+			surcharge_refresh();
+		}
+		
+		function surcharge_refresh() {
+			// Initialize extra fields
+			ec = ec || {};
+			ec.order = ec.order || {};
+			ec.order.extraFields = ec.order.extraFields || {};
+
+			ec.order.extraFields.surcharge = {
+				'value': 'Custom charge',
+				'options': [{ 
+					"title": "Credit Card Processing Fee",
+					"surcharge": 5
+				},
+				],
+				'surchargeShortName': {
+					'name': 'ProcessingFee',
+					'showSurchargePercentValue': false
+				},
+				'surchargeType': 'PERCENT',
+				'showZeroSurchargeInTotal': false
+			};
+			Ecwid.refreshConfig();	
+		}
 	}
 });
