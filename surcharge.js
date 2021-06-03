54810d04b805c31739c9371994028667f30ffe9c
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-03 09:35:36 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -8,25 +8,6 @@ Ecwid.OnPageLoaded.add(function(page) {
 		var widgets = Ecwid.getInitializedWidgets();
 		console.log(widgets);
 		
-		// Initialize extra fields
-		ec = ec || {};
-		ec.order = ec.order || {};
-		ec.order.extraFields = ec.order.extraFields || {};
-		// Set order surcharge
-		ec.order.extraFields.surcharge = {
-		    'value': 'Credit Card Processing Fee',
-		    "options": [
-		    { 
-			"title": "Custom charge",
-			"surcharge": 5
-		    },
-		    ],
-		    "surchargeShortName": {
-			"name": "Surcharge",
-			 "showSurchargePercentValue": false
-		    },
-		    'surchargeType': 'PERCENT'
-		};
-		Ecwid.refreshConfig();
+		
 	}
 });
