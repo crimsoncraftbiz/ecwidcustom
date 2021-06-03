bdce95b33f402863e719696e3274e08565f5214b
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-03 10:01:34 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -1,10 +1,32 @@
 Ecwid.OnPageLoaded.add(function(page) {
-	console.log("Current page is of type: " + page.type);
+	console.log('The custom app loaded');
 	
 	if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
 		//alert this app loaded
-		console.log("Current page is of type: " + page.type);
-		
-		
+		console.log('The custom app works in payment page!!');
 	}
 });
+
+// Initialize extra fields
+		ec = ec || {};
+		ec.order = ec.order || {};
+		ec.order.extraFields = ec.order.extraFields || {};
+
+		ec.order.extraFields.surcharge = {
+			'title': 'Tips',
+			'value': 'Custom charge',
+			'type': 'toggleButtonGroup',
+			"options": [
+			{ 
+				"title": "Credit Card Processing Fee",
+				"surcharge": 5
+			},
+			],
+			
+			'surchargeShortName': {
+				'name': 'ProcessingFee',
+				'showSurchargePercentValue': false,
+			},
+			'surchargeType': 'PERCENT'
+		};
+		Ecwid.refreshConfig();
