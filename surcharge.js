531147edf613579c416cd9991fa3df04a5590293
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-03 10:14:32 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -4,10 +4,10 @@ Ecwid.OnPageLoaded.add(function(page) {
 	if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
 		//alert this app loaded
 		console.log('The custom app works in payment page!!');
-	}
-});
-
-// Initialize extra fields
+	
+	
+	
+		// Initialize extra fields
 		ec = ec || {};
 		ec.order = ec.order || {};
 		ec.order.extraFields = ec.order.extraFields || {};
@@ -30,3 +30,6 @@ Ecwid.OnPageLoaded.add(function(page) {
 			'surchargeType': 'PERCENT'
 		};
 		Ecwid.refreshConfig();
+	
+	}
+});
