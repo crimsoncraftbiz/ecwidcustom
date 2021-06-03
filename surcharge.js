196a2e6e4880b55e9d78c0c7a03cac919cedeefd
crimsoncraftbiz <84789073+crimsoncraftbiz@users.noreply.github.com>
2021-06-03 10:24:46 +0800

    Update surcharge.js

--- surcharge.js
+++ surcharge.js
@@ -1,9 +1,9 @@
 Ecwid.OnPageLoaded.add(function(page) {
-	console.log('The custom app loaded');
+	console.log("Current page is of type: " + page.type);
 	
 	if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
 		//alert this app loaded
-		console.log('The custom app works in payment page!!');
+		console.log("Current page is of type: " + page.type);
 	
 	
 	
